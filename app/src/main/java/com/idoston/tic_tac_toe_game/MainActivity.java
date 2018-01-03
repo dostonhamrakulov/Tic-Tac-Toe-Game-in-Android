9c08dc6a3c1491682d1f94212182303e73e8a672
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-01-03 16:46:12 +0200

    Project is created

---
+++ app/src/main/java/com/idoston/tic_tac_toe_game/MainActivity.java
@@ -0,0 +1,13 @@
+package com.idoston.tic_tac_toe_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
