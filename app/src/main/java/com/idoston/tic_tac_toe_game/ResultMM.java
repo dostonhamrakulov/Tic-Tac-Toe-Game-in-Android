cbbe9eca21ffb02b529ab213dddcae2b60840bb6
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-01-03 16:59:09 +0200

    Developed all parts almost

---
+++ app/src/main/java/com/idoston/tic_tac_toe_game/ResultMM.java
@@ -0,0 +1,22 @@
+package com.idoston.tic_tac_toe_game;
+
+public class ResultMM {
+
+    String[] matrix; // The state of the board at a given stage
+    int score; // The score associated with the board (win = +1, lose = -1, else = 0)
+    int depth; // The depth in the tree structure in which this state exists
+
+    public ResultMM(String[] matrix, int score, int depth) {
+        this.matrix = matrix;
+        this.score = score;
+        this.depth = depth;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setMatrix(String[] matrix) {
+        this.matrix = matrix;
+    }
+}
